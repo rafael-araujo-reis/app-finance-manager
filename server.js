ec0880ecefd34517346e8a3b627a668fe7a28c0a
Rafael Reis <rafa.araujoreis@gmail.com>
2023-05-16 08:56:43 -0300

    feat: add heroku config

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const appName = "app-finance-manager";
+const outputPath = `${__dirname}/dist/${{ appName }}`;
+
+app.use(express.static(outputPath));
+app.get("/*", (req, res) => {
+  res.sendFile(`${outputPath}/index.html`);
+});
+
+app.listen(process.env.PORT);
